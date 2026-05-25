1dc363b13a7e5609ea0541a8b53862de07810798
Marvin95122 <marvincruz868@gmail.com>
2026-05-24 23:26:59 -0600

    actualizacion HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         View::composer('dashboard', function ($view) {
             $view->with('clima', app(OpenMeteoService::class)->obtenerClimaActual());
         });
